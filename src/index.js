2f353003b8a6236fdb0a7fddb72c73d807b44a07
Ash Luchowa <ashwinluchowa@gmail.com>
2025-09-20 22:30:35 +0800

    Fork challenge app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client"; // Note the change in import path
+import Lifecycle from "./LifeCycle";
 
-const App = () => <h1>Hello, React!</h1>;
+function ItemList({ items }) {
+  // Your code goes here to return JSX
+  return (
+    <ol>
+      {items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ol>
+  );
+}
 
-createRoot(document.getElementById("root")).render(<App />);
+// Usage example
+// const App = () => {
+//   const items = [
+//     { id: 1, title: "First Item" },
+//     { id: 2, title: "Second Item" },
+//     { id: 3, title: "Third Item" },
+//   ];
+
+//   return <ItemList items={items} />;
+// };
+
+function App() {
+  const items = [
+    { id: 1, title: "First Item" },
+    { id: 2, title: "Second Item" },
+    { id: 3, title: "Third Item" },
+  ];
+
+  return (
+    <>
+      <Lifecycle />
+      <ItemList items={items} />
+      {/* <UserProfile
+        firstName="John"
+        lastName="Doe"
+        age={30}
+        isStudent={false}
+        skills={["JavaScript", "React", "CSS"]}
+      /> */}
+    </>
+  );
+}
+
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
